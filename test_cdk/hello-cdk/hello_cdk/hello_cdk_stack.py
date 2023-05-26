be65eb1e2e97126edbf209fc477c6372924f7d6a
Michel Lacle <mlacle@progressrail.com>
2023-05-26 13:10:06 -0400

    create bucket and role

--- test_cdk/hello-cdk/hello_cdk/hello_cdk_stack.py
+++ test_cdk/hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Stack
 )
 import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_iam as iam
 
 from constructs import Construct
 
@@ -12,9 +13,9 @@ class HelloCdkStack(Stack):
 
         bucket = s3.Bucket(self, "michel-is-awesome-today", bucket_name='michel-is-awesome',versioned=True)
 
-        # another bucket to store my music files
-        bucket2 = s3.Bucket(self, "michel-is-awesome-today2", bucket_name='michel-is-awesome2',versioned=True)
+        # now create a variable called role and assign it to the iam role used for s3 access
+        role = iam.Role(self, "s3_full_access", role_name = 's3-full-access', assumed_by=iam.ServicePrincipal("s3.amazonaws.com"))
 
-        # for loop to create 10 buckets
-        for i in range(10):
-            bucket = s3.Bucket(self, "michel-is-awesome-today"+str(i), bucket_name='michel-is-awesome'+str(i),versioned=True)
+        # now attach s3 full admin mpolicey to the role
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"))
+
