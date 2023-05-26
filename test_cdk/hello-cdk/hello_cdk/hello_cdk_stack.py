9b67b03277e3b22fda40405cf08440ff565523ad
Michel Lacle <mlacle@progressrail.com>
2023-05-26 11:50:57 -0400

    first bucket!

--- test_cdk/hello-cdk/hello_cdk/hello_cdk_stack.py
+++ test_cdk/hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -1,8 +1,8 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    Stack
 )
+import aws_cdk.aws_s3 as s3
+
 from constructs import Construct
 
 class HelloCdkStack(Stack):
@@ -10,10 +10,4 @@ class HelloCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "michel-is-awesome-today", bucket_name='michel-is-awesome',versioned=True)
